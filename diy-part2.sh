23cbe802960516d8c35ee190c4312520ee44dac0
Austin <105551986+AustinEdSun@users.noreply.github.com>
2023-03-24 10:49:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,13 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.249/g' package/base-files/files/bin/config_generate
 
+# themes添加（svn co 命令意思：指定版本如https://github）
+rm -rf ./feeds/luci/themes/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+
+# 添加额外软件包
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+
 # 取消原主题luci-theme-bootstrap 为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
@@ -21,3 +28,8 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 
 # 删除原默认主题
 rm -rf package/lean/luci-theme-bootstrap
+
+# Modify NTP Server
+sed -i "s/0.openwrt.pool.ntp.org/ntp.aliyun.com/g" package/base-files/files/bin/config_generate
+sed -i "s/1.openwrt.pool.ntp.org/cn.ntp.org.cn/g" package/base-files/files/bin/config_generate
+sed -i "s/2.openwrt.pool.ntp.org/cn.pool.ntp.org/g" package/base-files/files/bin/config_generate
