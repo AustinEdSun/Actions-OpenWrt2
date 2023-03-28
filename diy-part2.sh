14d7283cb02bbba310a219dea5b8c2273e194382
Austin <105551986+AustinEdSun@users.noreply.github.com>
2023-03-28 23:03:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,8 +10,12 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# 自定义定制选项
+NET="package/base-files/files/bin/config_generate"
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
+
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.249/g' package/base-files/files/bin/config_generate
+sed -i 's#192.168.1.1#192.168.1.249#g' $NET
 
 # 删除原默认主题
 rm -rf package/lean/luci-theme-argon
@@ -31,6 +35,35 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# ●●●●●●●●●●●●●●●●●●●●●●●●定制部分●●●●●●●●●●●●●●●●●●●●●●●● #
+
+cat >> $ZZZ <<-EOF
+# 设置旁路由模式
+uci set network.lan.gateway='192.168.1.250'                  # 旁路由设置 IPv4 网关
+uci set network.lan.dns='202.101.172.35 114.114.114.114'     # 旁路由设置 DNS(多个DNS要用空格分开)
+uci set network.lan.delegate='0'                             # 去掉LAN口使用内置的 IPv6 管理(若用IPV6请把'0'改'1')
+uci set dhcp.@dnsmasq[0].filter_aaaa='1'                     # 禁止解析 IPv6 DNS记录(若用IPV6请把'1'改'0')
+uci set dhcp.lan.ignore='1'                                  # 旁路由关闭DHCP功能
+uci delete network.lan.type                                  # 旁路由桥接模式-禁用
+# 旁路IPV6需要全部禁用
+uci set network.lan.ip6assign=''                             # IPV6分配长度-禁用
+uci set dhcp.lan.ra=''                                       # 路由通告服务-禁用
+uci set dhcp.lan.dhcpv6=''                                   # DHCPv6 服务-禁用
+uci set dhcp.lan.ra_management=''                            # DHCPv6 模式-禁用
+# 如果有用IPV6的话,可以使用以下命令创建IPV6客户端(LAN口)（去掉全部代码uci前面#号生效）
+#uci set network.ipv6=interface
+#uci set network.ipv6.proto='dhcpv6'
+#uci set network.ipv6.ifname='eth0'
+#uci set network.ipv6.reqaddress='try'
+#uci set network.ipv6.reqprefix='auto'
+#uci set firewall.@zone[0].network='lan ipv6'
+EOF
+
+# 修改退出命令到最后
+sed -i '/exit 0/d' $ZZZ && echo "exit 0" >> $ZZZ
+
+# ●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●●● #
 
 # Modify NTP Server
 sed -i "s/0.openwrt.pool.ntp.org/ntp.aliyun.com/g" package/base-files/files/bin/config_generate
